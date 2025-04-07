13cf1a7c5485623c22fc6b87a0788ec1c7f3a7ea
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2025-04-07 15:08:04 +0600

    rp

---
+++ create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE people (
+    id INTEGER,
+    tag TEXT,
+    name TEXT,
+    age INTEGER,
+    balance INTEGER,
+    is_admin BOOLEAN
+);
+
+INSERT INTO people (id, tag, name, age, balance, is_admin)
+VALUES (3, 'sally', 'Sally Wagoner', 18, 100, true);
+
+SELECT * FROM people;
